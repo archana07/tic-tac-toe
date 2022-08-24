06225649d26605265ee97033979792c3aa71f54f
archana07 <archana714@outlook.com>
2022-08-24 13:23:31 +0530

    Use double quotes

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+    printWidth: 120,
+    singleQuote: false,
+    useTabs: false,
+    tabWidth: 4,
+    semi: true,
+    trailingComma: 'none',
+    bracketSpacing: false,
+    bracketSameLine: true,
+    arrowParens: 'always'
+};
